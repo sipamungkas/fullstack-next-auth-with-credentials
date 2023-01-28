71363c865814e055db5034b2d51fe74f0337d303
Ragil Pamungkas <sipamungkas.dev@gmail.com>
2023-01-28 23:05:37 +0700

    feat: add protected route for ssr and csr

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import { signIn } from "next-auth/client";
+import { useRouter } from "next/router";
 
 import classes from "./auth-form.module.css";
 
@@ -26,6 +27,7 @@ function AuthForm() {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
 
   async function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
@@ -43,6 +45,10 @@ function AuthForm() {
         email: enteredEmail,
         password: enteredPassword,
       });
+
+      if (!result.error) {
+        router.replace("/profile");
+      }
 
       console.log(result);
     } else {
